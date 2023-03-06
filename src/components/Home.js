5445f7d6a6d812b9d16b2cff4c6aff7d4481481c
mika <mikagivner123@gmail.com>
2023-03-06 23:46:45 +0200

    Home design

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,24 +5,22 @@ import CardPlace from "./Card-place";
 import { SaveOptionContext } from "./StateContext";
 import {
   TryAgain,
-  HomeHead,
-  FavouritesStyle,
-  InputTitle,
+  SearchNav,
   HomeStyle,
-  ChooseBox,
-  DisplayRow,
+  Message,
   Submit,
   WrapperPlaces,
+  HomeTitle,
+  SearchMenu,
+  SelectStyle,
 } from "../style";
 import "./style.css";
 import "./SpinnerStyle.css";
+import { Button } from "../style/Submit";
 
 export default function Home() {
   const {
     saveOptionn,
-    setSaveOptionn,
-    userName,
-    setUserName,
     isLearn,
     setIsLearn,
     isLoading,
@@ -34,11 +32,12 @@ export default function Home() {
   } = useContext(SaveOptionContext);
   let counterPlaces = 0;
   // const [isLearn, setIsLearn] = useState(false);
-  const [event, setEvent] = useState("--Where do U wanna go--");
-  const [area, setArea] = useState("--What is your area--");
-  const [currentEvent, setCurrentEvent] = useState("--Where do U wanna go--");
-  const [currentArea, setCurrentArea] = useState("--What is your area--");
+  const [event, setEvent] = useState("");
+  const [area, setArea] = useState("");
+  const [currentEvent, setCurrentEvent] = useState("");
+  const [currentArea, setCurrentArea] = useState("");
 
+  const [message, setMessage] = useState("");
   const newEvent = (e) => {
     setEvent(e.target.value);
   };
@@ -47,17 +46,23 @@ export default function Home() {
   };
 
   const [showData, setShowData] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [searchNow, setSearchNow] = useState(false);
+  const [hasData, setHasData] = useState(false);
 
-  const MyData = async () => {
-    setCurrentEvent(event);
-    setCurrentArea(area);
-    if (
-      currentEvent !== "--Where do U wanna go--" &&
-      currentArea !== "--What is your area--"
-    ) {
+  const MyData = async (e) => {
+    e.preventDefault();
+    setMessage("");
+    if (event !== "" && area !== "") {
+      setHasData(true);
+    }
+    if (event === "" || area === "") {
+      setMessage("Please select event and area!");
+    } else if (currentEvent === event && currentArea === area) {
+      setMessage("You have already search this!");
+    } else {
+      setCurrentEvent(event);
+      setCurrentArea(area);
       setSearchNow(true);
       try {
         setIsLoading(true);
@@ -82,13 +87,11 @@ export default function Home() {
       `https://63fcb7158ef914c5559dbaa5.mockapi.io/api/sa1/company`
     );
 
-    // const currentUser = localStorage.getItem("logIn");
-
     if (!res.data[e.target.id - 1].usersSave.includes(currentUser)) {
       const updatedUsersSave = [
         ...res.data[e.target.id - 1].usersSave,
         currentUser,
-      ]; // add currentUser as a string to the existing array
+      ];
       const response = await axios.put(
         `https://63fcb7158ef914c5559dbaa5.mockapi.io/api/sa1/company/${e.target.id}`,
         { usersSave: updatedUsersSave }
@@ -110,119 +113,86 @@ export default function Home() {
     setIsLoading(false);
     console.log(e.target.id);
   }
-  const [isDisable, setIsDisable] = useState(true);
 
-  useEffect(() => {
-    event !== "--Where do U wanna go--" &&
-      area !== "--What is your area--" &&
-      setIsDisable(false);
-  }, [MyData, Submit]);
   function deleteSave() {}
   return (
-    <div>
-      <FavouritesStyle>
-        {isLearn && <LearnMore about={learnAbout} placeTitle={namePlace} />}
+    <HomeStyle>
+      {isLearn && <LearnMore about={learnAbout} placeTitle={namePlace} />}
+      {/* {currentUser.length !== 0 && <div>hey {currentUser}</div>} */}
 
-        <HomeStyle>
-          {currentUser.length !== 0 && <div>hey {currentUser}</div>}
-          {/* <HomeHead></HomeHead> */}
-          <h1>What is your next adventure?</h1>
-          <DisplayRow>
-            <ChooseBox>
-              <InputTitle>I wanna go to:</InputTitle>
+      <SearchNav>
+        <HomeTitle>Find your next trip!</HomeTitle>
+        <SearchMenu onSubmit={MyData}>
+          <SelectStyle value={event} name="event" onChange={newEvent}>
+            <option hidden selected>
+              Select event
+            </option>
+            <option className="n" value="walking trials">
+              Hiking trails
+            </option>
+            <option value="camping">Camping</option>
+            <option value="picnic">Picnic</option>
+            <option value="dogs friendly">Dogs Friendly</option>
+            <option value="bikes trails">Bikes Trails</option>
+            <option value="water experience">Water Experience</option>
+            <option value="for the Story">For The "Story"</option>
+          </SelectStyle>
+          <SelectStyle value={area} name="area" onChange={newArea}>
+            <option hidden selected>
+              Select area
+            </option>
+            <option value="north">North</option>
+            <option value="center">Center</option>
+            <option value="south">South</option>
+          </SelectStyle>
+          <Button type="submit">Search</Button>
+        </SearchMenu>
+        <Message>{message}</Message>
+      </SearchNav>
+      {hasData && (
+        <SearchNav>
+          {isError && <TryAgain>page not found</TryAgain>}
 
-              <select
-                value={event}
-                name="event"
-                className="selectInput"
-                onChange={newEvent}
-              >
-                <option hidden selected>
-                  --Where do U wanna go--
-                </option>
-                <option className="optiontInput" value="walking trials">
-                  Hiking trails
-                </option>
-                <option value="camping">Camping</option>
-                <option value="picnic">Picnic</option>
-                <option value="dogs friendly">Dogs Friendly</option>
-                <option value="bikes trails">Bikes Trails</option>
-                <option value="water experience">Water Experience</option>
-                <option value="for the Story">For The "Story"</option>
-              </select>
-            </ChooseBox>
-            <ChooseBox>
-              <InputTitle>My Area Is:</InputTitle>
+          {isLoading && !isError && (
+            <div className="lds-ellipsis">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          )}
 
-              <select
-                value={area}
-                name="area"
-                className="selectInput"
-                onChange={newArea}
-              >
-                <option hidden selected>
-                  --What is your area--
-                </option>
-                <option value="north">North</option>
-                <option value="center">Center</option>
-                <option value="south">South</option>
-              </select>
-            </ChooseBox>
-          </DisplayRow>
-          {/* {event !== "--Where do U wanna go--" &&
-          area !== "--What is your area--"
-            ? setIsDisable(false)
-            : setIsDisable(true)} */}
+          {!isLoading && (
+            <WrapperPlaces>
+              {showData.map((company) => {
+                if (
+                  company.area === currentArea &&
+                  company.events.includes(currentEvent)
+                ) {
+                  return (
+                    <CardPlace
+                      key={company.id}
+                      id={company.id}
+                      title={company.name}
+                      saveOption={saveOptionn}
+                      SaveItem={SaveItem}
+                      background={company.img}
+                      LearnMore={Learn}
+                      deleteSave={deleteSave}
+                    >
+                      {counterPlaces++}
+                    </CardPlace>
+                  );
+                }
+              })}
 
-          <Submit disabled={isDisable} onClick={MyData}>
-            Search Places
-          </Submit>
-          <div>
-            {isError && <div>page not found</div>}
-            {/*TODO: ADD SPINNER INSTED OF LOADING */}
-            {isLoading && !isError && (
-              // <div style={{ color: "red" }}>Loading...</div>
-              <div className="lds-ellipsis">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-              </div>
-            )}
-
-            {!isLoading && (
-              <WrapperPlaces>
-                {showData.map((company) => {
-                  if (
-                    company.area === currentArea &&
-                    company.events.includes(currentEvent)
-                  ) {
-                    return (
-                      <CardPlace
-                        key={company.id}
-                        id={company.id}
-                        title={company.name}
-                        saveOption={saveOptionn}
-                        SaveItem={SaveItem}
-                        background={company.img}
-                        LearnMore={Learn}
-                        deleteSave={deleteSave}
-                      >
-                        {counterPlaces++}
-                      </CardPlace>
-                    );
-                  }
-                })}
-
-                {/* {!searchNow && <div>Please first pick adventure </div>} */}
-                {counterPlaces === 0 && searchNow && (
-                  <TryAgain>places are not found, try again</TryAgain>
-                )}
-              </WrapperPlaces>
-            )}
-          </div>
-        </HomeStyle>
-      </FavouritesStyle>
-    </div>
+              {counterPlaces === 0 && searchNow && (
+                <TryAgain>places are not found, try again</TryAgain>
+              )}
+            </WrapperPlaces>
+          )}
+        </SearchNav>
+      )}
+    </HomeStyle>
   );
 }
